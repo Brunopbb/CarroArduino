507c228942a0263fae04caf6bbc71cd1e58676a8
Bruno Almeida <almeidabruno99@gmail.com>
2020-04-20 23:04:35 -0300

    Software

---
+++ Software/Software.ino
@@ -0,0 +1,30 @@
+
+
+int pin2 = 2;
+int pin7 = 7;
+
+
+void setup() {
+	
+    Serial.begin(9600);
+
+    pinMode(pin2, INPUT);
+    pinMode(pin7, INPUT);
+
+
+}
+
+void loop() {
+    
+    int ch3 = pulseIn(pin7, HIGH, 50000);
+    int ch1 = pulseIn(pin2, HIGH, 50000);
+
+    
+    
+    channel1(ch1);
+
+    channel3(ch3);
+    
+        
+
+}
